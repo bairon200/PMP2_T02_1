9b99f917726a49de0406fa8942ca0eb932da908f
bairon200 <91520883+bairon200@users.noreply.github.com>
2022-07-10 12:03:24 -0600

    Agregar archivos de proyecto.

---
+++ PMP2_T02/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PMP2_T02
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAfrica_Clicked(object sender, EventArgs e)
+        {
+            var am = new views.list(0);
+            await Navigation.PushAsync(am);
+        }
+
+        private async void btnAmerica_Clicked(object sender, EventArgs e)
+        {
+            var am = new views.list(1);
+            await Navigation.PushAsync(am);
+
+        }
+
+        private async void btnAsia_Clicked(object sender, EventArgs e)
+        {
+            var am = new views.list(2);
+            await Navigation.PushAsync(am);
+
+        }
+
+        private async void btnEuropa_Clicked(object sender, EventArgs e)
+        {
+            var am = new views.list(3);
+            await Navigation.PushAsync(am);
+        }
+
+        private async void btnOseania_Clicked(object sender, EventArgs e)
+        {
+            var am = new views.list(4);
+            await Navigation.PushAsync(am);
+        }
+
+    }
+}
---
+++ PMP2_T02/controller/paises.cs
@@ -0,0 +1,91 @@
+﻿using Newtonsoft.Json;
+using PMP2_T02.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMP2_T02.controller
+{
+    public class paises
+    {
+        
+
+            public async static Task<List<contructor.Root>> ObtenerAmerica()
+            {
+                List<contructor.Root> listapaises = new List<contructor.Root>();
+                using (HttpClient client = new HttpClient())
+                {
+                    var response = await client.GetAsync("https://restcountries.com/v3.1/region/Americas");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var contenido = response.Content.ReadAsStringAsync().Result;
+                        listapaises = JsonConvert.DeserializeObject<List<contructor.Root>>(contenido);
+                    }
+                }
+                return listapaises;
+            }
+
+            public async static Task<List<contructor.Root>> ObtenerEuropa()
+            {
+                List<contructor.Root> listapaises = new List<contructor.Root>();
+                using (HttpClient client = new HttpClient())
+                {
+                    var response = await client.GetAsync("https://restcountries.com/v3.1/region/Europe");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var contenido = response.Content.ReadAsStringAsync().Result;
+                        listapaises = JsonConvert.DeserializeObject<List<contructor.Root>>(contenido);
+                    }
+                }
+                return listapaises;
+            }
+
+            public async static Task<List<contructor.Root>> ObtenerAfrica()
+            {
+                List<contructor.Root> listapaises = new List<contructor.Root>();
+                using (HttpClient client = new HttpClient())
+                {
+                    var response = await client.GetAsync("https://restcountries.com/v3.1/region/Africa");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var contenido = response.Content.ReadAsStringAsync().Result;
+                        listapaises = JsonConvert.DeserializeObject<List<contructor.Root>>(contenido);
+                    }
+                }
+                return listapaises;
+            }
+
+            public async static Task<List<contructor.Root>> ObtenerOceania()
+            {
+                List<contructor.Root> listapaises = new List<contructor.Root>();
+                using (HttpClient client = new HttpClient())
+                {
+                    var response = await client.GetAsync("https://restcountries.com/v3.1/region/Oceania");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var contenido = response.Content.ReadAsStringAsync().Result;
+                        listapaises = JsonConvert.DeserializeObject<List<contructor.Root>>(contenido);
+                    }
+                }
+                return listapaises;
+            }
+            public async static Task<List<contructor.Root>> ObtenerAsia()
+            {
+                List<contructor.Root> listapaises = new List<contructor.Root>();
+                using (HttpClient client = new HttpClient())
+                {
+                    var response = await client.GetAsync("https://restcountries.com/v3.1/region/Asia");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var contenido = response.Content.ReadAsStringAsync().Result;
+                        listapaises = JsonConvert.DeserializeObject<List<contructor.Root>>(contenido);
+                    }
+                }
+                return listapaises;
+            }
+
+        
+    }
+}
